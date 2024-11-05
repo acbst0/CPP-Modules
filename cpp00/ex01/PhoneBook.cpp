a7b685cadb40547b896e3e7c167dcbc5cdc99df7
Ahmet Cemil Bostanoglu <abostano@k1m23s07.42istanbul.com.tr>
2024-11-05 10:50:00 +0300

    Exit is not neccessary

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -25,24 +25,14 @@ int	PhoneBook::add(PhoneBook *pb)
 
 	std::cout << "First name of new contact :" << std::endl;
 	std::getline(std::cin, firstname);
-	if (firstname.compare("EXIT") == 0)
-		return (1);
 	std::cout << "Last name of new contact :" << std::endl;
 	std::getline(std::cin, lastname);
-	if (lastname.compare("EXIT") == 0)
-		return (1);
 	std::cout << "Nickname of new contact :" << std::endl;
 	std::getline(std::cin, nickname);
-	if (nickname.compare("EXIT") == 0)
-		return (1);
 	std::cout << "Phone Number of new contact :" << std::endl;
 	std::getline(std::cin, phonenum);
-	if (phonenum.compare("EXIT") == 0)
-		return (1);
 	std::cout << "Dark secret of new contact :" << std::endl;
 	std::getline(std::cin, darkness);
-	if (darkness.compare("EXIT") == 0)
-		return (1);
 
 	Contact newcnt(firstname, lastname, nickname, phonenum, darkness);
 	pb->cnt[pb->_input_times % MAX_CONT] = newcnt;
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -33,14 +33,34 @@ void	printCT(Contact *ct, int i)
 	printline(nickname);
 	printline(phonenum);
 	std::cout << std::endl;
-	std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
+	//std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
 }
 
+void	searchindex(PhoneBook *pb)
+{
+	std::string index;
+	int			i;
+
+	std::cout << "Plz enter the index of the contact you want to see : " << std::endl;
+	std::getline(std::cin, index);
+	i = std::stoi(index);
+	if (i < 1 || i > 8 || pb->cnt[i - 1].getEmpty() == false)
+	{
+		std::cout << "Invalid index" << std::endl;
+		return ;
+	}
+	std::cout << "First Name : " << pb->cnt[i - 1].getFirstName() << std::endl;
+	std::cout << "Last Name : " << pb->cnt[i - 1].getLastName() << std::endl;
+	std::cout << "Nick Name : " << pb->cnt[i - 1].getNickName() << std::endl;
+	std::cout << "Phone Number : " << pb->cnt[i - 1].getPhoneNum() << std::endl;
+	std::cout << "Darkest secret : " << pb->cnt[i - 1].getDarkness() << std::endl;
+
+}
 void	printPB(PhoneBook *pb)
 {
 	std::cout << std::endl;
 	std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
-	std::cout << "|        ID|First Name| Last Name| Nick Name|    Number|" << std::endl;
+	std::cout << "|     INDEX|First Name| Last Name| Nick Name|    Number|" << std::endl;
 	std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
 	if (pb == NULL)
 		return ;
@@ -50,6 +70,7 @@ void	printPB(PhoneBook *pb)
 		{
 			printCT(&pb->cnt[i], i);
 		}
+		searchindex(pb);
 	}
 }
 
