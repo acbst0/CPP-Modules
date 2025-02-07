b69d9486e0fa02e2e22bbfb69c0f0d9c2d6fcc12
Ahmet Cemil Bostanoğlu <abostano@k1m35s01.42istanbul.com.tr>
2025-02-07 12:57:10 +0300

    Say hello to cpp05

--- cpp05/ex00/Bureucrat.hpp
+++ cpp05/ex00/Bureucrat.hpp
@@ -3,6 +3,17 @@
 
 # include <iostream>
 
+class Bureucrat
+{
+private:
+	const std::string _name;
+	int _grade;
+public:
+	Bureucrat();
+	Bureucrat(const Bureucrat& ref);
+	~Bureucrat();
+	Bureucrat& operator=(const Bureucrat& ref);
+};
 
 
 #endif
