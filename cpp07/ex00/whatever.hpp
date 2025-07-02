c93e7eab75940d710b252bbfcbf8aa835a813541
Ahmet Cemil Bostanoğlu <abostano@k1m22s07.42istanbul.com.tr>
2025-07-02 18:59:29 +0300

    CPP07ex00 is over

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -3,4 +3,30 @@
 
 # include <iostream>
 
+template <typename T>
+const T& min(const T &a, const T &b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+const T& max(const T &a, const T &b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
 #endif
