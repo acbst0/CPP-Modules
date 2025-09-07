496c67f63c312e3418c36730cb480b171cf2e9c1
acbst0 <ahmetcemilbostanoglu@yahoo.com>
2025-09-07 15:50:05 +0300

    Fixing issues

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -223,6 +223,16 @@ bool isValidDate(const std::string& date)
 	if (month < 1 || month > 12 || day < 1 || day > 31)
 		return false;
 
-	// Daha fazla kontrol (örneğin Şubat 30, vs.) istersen ekleyebilirsin
+	if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+		return false;
+	
+	if (month == 2)
+	{
+		bool isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		if (isLeap && day > 29)
+			return false;
+		if (!isLeap && day > 28)
+			return false;
+	}
 	return true;
 }
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -4,7 +4,7 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cerr << "Bad input!" << std::endl;
+		std::cerr << "Error: could not open file." << std::endl;
 		return 1;
 	}
 
